80bad027427663a504eb0ad4dc72c8d5589f9141
KoFFein4iK <koffein4ik@gmail.com>
2019-03-26 23:27:34 +0300

    first factory created

--- src/ClassEditor.java
+++ src/ClassEditor.java
@@ -5,6 +5,9 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -25,16 +28,26 @@ public class ClassEditor extends Application {
     @Override
     public void start(Stage primaryStage) {
         Pane root = new Pane();
-        //root.setPadding(new Insets(10));
         root.setPadding(new Insets(10));
-        //root.setHgap(40);
-        //root.setVgap(10);
         Class<Airport> Airp = Airport.class;
         Field[] declaredFields = Airp.getDeclaredFields();
         for (Field field : declaredFields)
         {
             System.out.println(field);
         }
+        declaredFields[1].setAccessible(true);
+        Airport Heathrow = new Airport();
+        Class<?> h = Heathrow.getClass();
+        try {
+            Field location = h.getDeclaredField("location");
+            String loc = "London";
+            location.set(Heathrow, loc);
+        }
+        catch (Exception ex)
+        {
+
+        };
+        System.out.println(Heathrow.location);
         ArrayList<String> classNames = new ArrayList<String>();
         classNames.add(Airport.class.getName());
         classNames.add(AircrewMember.class.getName());
@@ -47,9 +60,17 @@ public class ClassEditor extends Application {
         classNames.add(Pilot.class.getName());
         ObservableList<String> classes = FXCollections.observableArrayList(classNames);
         ListView<String> ClassView = new ListView<String>(classes);
+        ObservableList<String> createdClasses = FXCollections.observableArrayList();
+        ListView<String> createdClassesView = new ListView<String>(createdClasses);
         ClassView.setPrefHeight(400);
+        ClassView.setPrefWidth(200);
         ClassView.setLayoutX(10);
         ClassView.setLayoutY(10);
+        createdClassesView.setPrefHeight(400);
+        createdClassesView.setLayoutX(250);
+        createdClassesView.setLayoutY(10);
+        createdClasses.add("Airbus");
+        createdClasses.add("John");
         primaryStage.setTitle("Class Editor");
         primaryStage.centerOnScreen();
         Button btn1 = new Button("Add");
@@ -61,7 +82,7 @@ public class ClassEditor extends Application {
         btn2.setPrefWidth(80);
         btn2.setLayoutX(420);
         btn2.setLayoutY(500);
-        root.getChildren().addAll(ClassView, btn1, btn2);
+        root.getChildren().addAll(ClassView, createdClassesView, /*editor,*/ btn1, btn2);
         Scene scene = new Scene(root, 350, 150);
         primaryStage.setScene(scene);
         primaryStage.setWidth(850);
---
+++ src/humanFactory.java
@@ -0,0 +1,2 @@
+public class humanFactory {
+}
